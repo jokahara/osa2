240a26842044fafe9390be000551317001cbb5b9
jokahara <38187890+jokahara@users.noreply.github.com>
2018-06-23 17:42:01 +0300

    Add files via upload

---
+++ maiden tiedot 2.11-2.13/src/App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import axios from 'axios'
+
+class App extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        countries: [],
+        filter: '',
+        showAll: true
+      }
+    }
+    
+    componentDidMount() {
+      axios
+        .get('https://restcountries.eu/rest/v2/all')
+        .then(response => {
+          console.log(response.data)
+          this.setState({ countries: response.data })
+        })
+    }
+    
+    handleFilterChange = (event) => {
+        this.setState({ filter: event.target.value })
+    }
+
+    clickHandler = (country) => {
+      return () => this.setState({ filter: country})
+    }
+    
+    render() {
+      const found = this.state.countries.filter(country => 
+        country.name
+          .toLowerCase()
+          .includes(this.state.filter.toLowerCase())
+      )
+
+      const countriesToShow = 
+        found.length > 10 
+          ? "too many matches, specify another filter"
+          : ( found.length === 1
+            ? <div>
+                <h1>{found[0].name}</h1>
+                <p>capital: {found[0].capital}</p>
+                <p>population: {found[0].population}</p>
+                <img src={found[0].flag} alt="Flag" height={200}/>
+              </div>
+            : found.map(country => 
+              <div 
+                onClick={this.clickHandler(country.name)} 
+                key={country.name}>
+                {country.name}
+              </div> 
+            )
+          )
+      
+      return (
+        <div>
+          <form>
+            find countries: 
+            <input 
+              onChange={this.handleFilterChange} 
+              value={this.state.filter}
+            />
+          </form>
+          {countriesToShow}
+        </div>
+      )
+    }
+}
+
+export default App
